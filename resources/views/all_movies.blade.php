8221a0e0b516651a1a22e7913ddd867a7a4c461c
Pietromuresu <muresupietro66@gmail.com>
2023-06-07 17:12:50 +0200

    Layout + Home page & all-movies page

---
+++ resources/views/all_movies.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+
+  <h1>Bravo!</h1>
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+
+<header class="position-fixed fixed-top ">
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Movies</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="{{route('home')}}">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('all_movies')}}">All Movies</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contacts</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+</header>
